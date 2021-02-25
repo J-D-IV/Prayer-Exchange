0303ed8903e26f72e0d42b0dd8ac062b67de39ba
JD <jd.havrilla1@gmail.com>
2021-02-24 19:42:09 -0700

    updated for heroku

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import Axios from 'axios';
-import Joy from './joy.jsx';
-import Affirmation from './affirmation.jsx'
-import Home from './home.jsx';
-import Login from './login.jsx';
-import Register from './Register.jsx';
+import Joy from './joy';
+import Affirmation from './affirmation'
+import Home from './home';
+import Login from './login';
+import Register from './Register';
 
 
 class App extends React.Component {
