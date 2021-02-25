34694072bf7c632d6f2115d0e4887b6c55ee3c21
JD <jd.havrilla1@gmail.com>
2021-02-24 20:16:30 -0700

    updated for heroku

--- database/index.js
+++ database/index.js
@@ -1,17 +1,5 @@
-// const { Pool } = require('pg');
 const { Client } = require('pg');
-// const client = new Pool({
-//   // user: 'postgres',
-//   // host: '18.217.213.246',
-//   // database: 'postgres',
-//   // host: 'localhost',
-//   user: 'fccseqhawfwmem',
-//   database: 'd739q6ilskd55v',
-//   host: 'ec2-54-164-241-193.compute-1.amazonaws.com',
-//   password: 'e9a6a5645eda0477e16384d6de9abb2f04662b2461de85fe57aefc6487f17112',
-//   // database: 'Spread Joy',
-//   port: 5432,
-// });
+
 const client = new Client({
   connectionString: process.env.DATABASE_URL,
   ssl: {
@@ -22,3 +10,14 @@ const client = new Client({
 client.connect();
 
 module.exports = client;
+
+// const { Pool } = require('pg');
+
+// const client = new Pool({
+//    user: 'postgres',
+//    host: '18.217.213.246',
+//    database: 'postgres',
+//    host: 'localhost',
+//    database: 'Spread Joy',
+//    port: 5432,
+// });
