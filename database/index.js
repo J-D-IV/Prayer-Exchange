c90d14c74b5178ebf6c64c26cb0869112d75b2f6
JD <jd.havrilla1@gmail.com>
2021-02-24 19:03:12 -0700

    updated for heroku

--- database/index.js
+++ database/index.js
@@ -1,11 +1,11 @@
 const { Pool } = require('pg');
 
 const client = new Pool({
-  user: 'postgres',
-  host: '18.217.213.246',
+  // user: 'postgres',
+  // host: '18.217.213.246',
   database: 'postgres',
-  // host: 'localhost',
-  // database: 'Spread Joy',
+  host: 'localhost',
+  database: 'Spread Joy',
   port: 5432,
 });
 
